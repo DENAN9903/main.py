a896314d8aab2263c443ea6ec7299261d4f84172
DENAN9903 <95846938+DENAN9903@users.noreply.github.com>
2021-12-09 15:06:45 +0700

    Update and rename README.md to main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import discord, os, keep_alive, asyncio, datetime, pytz
+
+from discord.ext import tasks, commands
+
+client = commands.Bot(
+
+  command_prefix=':',
+
+  self_bot=True
+
+)
+
+@client.event
+
+async def on_connect():
+
+  await client.change_presence(activity = discord.Streaming(name = "RANAME STREAMING KALIAN", url = "Link twitch.tv Channel Kalian"))
+
+keep_alive.keep_alive()
+
+client.run(os.getenv("TOKEN"), bot=False)
